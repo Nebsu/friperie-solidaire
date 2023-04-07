410986786781191412a8f3f2c6016fa4a141454d
Sebastien Zhou <Sebastien.4084@gmail.com>
2023-04-07 21:32:50 +0200

    Debut formulaire + transition ejs

---
+++ script.js
@@ -0,0 +1,40 @@
+let ejs = require("ejs");
+let express = require("express");
+let app = express();
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {});
+  return;
+});
+
+app.post("/", (req, res) => {
+  let name = req.body.name;
+  let surname = req.body.surname;
+  let email = req.body.email;
+  let password = req.body.password;
+  let confirm_password = req.body.confirmpassword;
+  if (email === "azfklazfazfb") {
+    // Verification si adresse email déjà utilisée
+  } else if (password != confirm_password || password.length < 8) {
+    // Verification mot de passe
+    res.render("index.ejs/", {});
+    console.log("Mot de passe invalide");
+    return;
+  } else {
+    // Ajout des données dans la base de données
+    res.render("index.ejs/", {});
+    console.log("Mot de passe valide");
+    console.log("Name: " + name);
+    console.log("Surname: " + surname);
+    console.log("Email: " + email);
+  }
+});
+
+app.listen(5500);
--- src/databasepg.js
+++ src/databasepg.js
@@ -1,19 +1,29 @@
+const express = require("express");
 const { Client } = require("pg");
+const app = express();
 const client = new Client({
   host: "localhost",
   user: "postgres",
   port: 4545,
   password: "root",
-  database: "Friperie",
+  database: "postgres",
 });
 
 client.connect();
 
-client.query(`SELECT * FROM stock`, (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err);
-  }
-  client.end();
+// Add a new user to the database*
+app.use(express.json());
+app.post("/users", (req, res) => {
+  const { name, surname, mail, password } = req.body;
+  client.query(
+    `INSERT INTO utilisateurs (name, surname, mail, password) VALUES ('${name}', '${surname}', '${mail}', '${password}')`,
+    (err, res) => {
+      if (!err) {
+        console.log("User added");
+      } else {
+        console.log(err);
+      }
+      client.end();
+    }
+  );
 });
