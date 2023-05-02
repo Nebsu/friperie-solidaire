b756f2e07f087fa708277ce4e06ff01fc6b7ea9d
Sebastien Zhou <Sebastien.4084@gmail.com>
2023-05-02 14:46:11 +0200

    Fix conflict

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let ejs = require("ejs");
-let express = require("express");
-let app = express();
+const ejs = require("ejs");
+const pg = require("pg");
+const express = require("express");
+const app = express();
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "root",
+  port: 5432,
+});
 
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
@@ -9,32 +17,136 @@ app.use("/img", express.static(__dirname + "public/img"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {});
+const database = [];
+class User {
+  constructor(name, surname, email, password) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+function getAllEmail() {
+  let emails = [];
+  for (let i = 0; i < database.length; i++) {
+    emails.push(database[i].email);
+  }
+  return emails;
+}
+
+function checkConnect(email, password) {
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].email === email && database[i].password === password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+app.get("/connection", function (req, res) {
+  const data = {
+    name: "",
+    surname: "",
+    email: "",
+    prenom: "",
+    connection: true,
+    inscription: false,
+  };
+  res.render("index.ejs", data);
   return;
 });
 
+app.get("/inscription", function (req, res) {
+  const data = {
+    name: "",
+    surname: "",
+    email: "",
+    prenom: "",
+    connection: false,
+    inscription: true,
+  };
+  res.render("index.ejs", data);
+  return;
+});
+
+app.get("/", (req, res) => {
+  const data = {
+    name: "",
+    surname: "",
+    email: "",
+    prenom: "",
+    connection: false,
+    inscription: false,
+  };
+  res.render("index.ejs", data);
+  return;
+});
+
+app.post("/inscription", (req, res) => {
+  const data = {
+    name: "",
+    surname: "",
+    email: "",
+    prenom: "",
+    connection: false,
+    inscription: true,
+  };
+  let name = req.body.name;
+  let surname = req.body.surname;
+  let email = req.body.email;
+  let password = req.body.password;
+  let confirm_password = req.body.confirmpassword;
+  console.log("Post Inscription");
+  if (getAllEmail().includes(email)) {
+    // check if email is already in database
+    res.render("index.ejs", data);
+    console.log("Email déjà utilisé");
+    return;
+  } else if (password != confirm_password || password.length < 8) {
+    // Verification mot de passe
+    pop_up = true;
+    res.render("index.ejs", data);
+    console.log("Mot de passe invalide");
+    return;
+  } else {
+    // Ajout des données dans la base de données
+    res.render("index.ejs", data);
+    const user = new User(name, surname, email, password);
+    database.push(user);
+    console.log(database);
+  }
+});
+
+app.post("/connection", (req, res) => {
+  const data = {
+    name: "",
+    surname: "",
+    email: "",
+    prenom: "",
+    connection: true,
+    inscription: false,
+  };
+  let email = req.body.email;
+  let surname = req.body.password;
+  console.log("Post Connection");
+  if (checkConnect(email, surname)) {
+    console.log("Connecté");
+  } else {
+    console.log("Erreur");
+  }
+  res.render("index.ejs", data);
+  // Partie connection
+});
 app.post("/", (req, res) => {
   let name = req.body.name;
   let surname = req.body.surname;
   let email = req.body.email;
   let password = req.body.password;
   let confirm_password = req.body.confirmpassword;
-  if (email === "azfklazfazfb") {
-    // Verification si adresse email déjà utilisée
-  } else if (password != confirm_password || password.length < 8) {
-    // Verification mot de passe
-    res.render("index.ejs/", {});
-    console.log("Mot de passe invalide");
-    return;
-  } else {
-    // Ajout des données dans la base de données
-    res.render("index.ejs/", {});
-    console.log("Mot de passe valide");
-    console.log("Name: " + name);
-    console.log("Surname: " + surname);
-    console.log("Email: " + email);
-  }
+  const database = [];
+  const data = { name: "", surname: "", email: "", prenom: "" };
+  console.log("Post");
 });
 
 app.listen(5501);
